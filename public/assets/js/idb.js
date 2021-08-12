d61caad11494b4590f5b854a0752464eb599341a
kirti18patel <kirti18patel@yahoo.com>
2021-08-11 22:54:30 -0400

    data fetched from indexed db and send to api

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -13,9 +13,9 @@ request.onsuccess = function(event) {
     db = event.target.result;
   
     // check if app is online, if yes run checkDatabase() function to send all local db data to api
-    // if (navigator.onLine) {
-    //   uploadPizza();
-    // }
+    if (navigator.onLine) {
+      uploadPizza();
+    }
   };
   
   request.onerror = function(event) {
@@ -31,3 +31,48 @@ function saveRecord(record) {
     // add record to your store with add method.
     pizzaObjectStore.add(record);
   }
+
+
+  function uploadPizza() {
+    // open a transaction on your pending db
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+  
+    // access your pending object store
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+  
+    // get all records from store and set to a variable
+    const getAll = pizzaObjectStore.getAll();
+  
+    getAll.onsuccess = function() {
+      // if there was data in indexedDb's store, let's send it to the api server
+      if (getAll.result.length > 0) {
+        fetch('/api/pizzas', {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(serverResponse => {
+            if (serverResponse.message) {
+              throw new Error(serverResponse);
+            }
+  
+            const transaction = db.transaction(['new_pizza'], 'readwrite');
+            const pizzaObjectStore = transaction.objectStore('new_pizza');
+            // clear all items in your store
+            pizzaObjectStore.clear();
+          })
+          .catch(err => {
+            // set reference to redirect back here
+            console.log(err);
+          });
+      }
+    };
+  }
+
+  
+// listen for app coming back online
+window.addEventListener('online', uploadPizza);
